c5ba8c4ea90506cfcbee5a13cd78ee9e73e080f7
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-02-02 15:34:16 -0500

    Added ground pound particles

---
+++ Assets/_SurvivalMars/Scripts/Enemies/BigEnemyGroundPound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GroundPound : UnityEngine.Events.UnityEvent { }
+public class BigEnemyGroundPound : MonoBehaviour
+{
+    public GroundPound m_groundHit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_groundHit.Invoke();
+    }
+}
