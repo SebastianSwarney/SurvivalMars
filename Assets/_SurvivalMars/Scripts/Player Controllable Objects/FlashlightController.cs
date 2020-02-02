87403d297c18ac47f37f1ae27716a8f5b2870f5a
SebastianSwarney <34040389+SebastianSwarney@users.noreply.github.com>
2020-02-02 10:00:11 -0500

    Prefab Updates

---
+++ Assets/_SurvivalMars/Scripts/Player Controllable Objects/FlashlightController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour
+{
+	private Light m_light;
+
+	public void ActivateLight()
+	{
+		m_light.enabled = true;
+	}
+
+	public void DeactivateLight()
+	{
+		m_light.enabled = false;
+	}
+
+	public void TriggerLightBurst()
+	{
+
+	}
+
+	public IEnumerator RunLightBrust()
+	{
+		float t = 0;
+	}
+}
