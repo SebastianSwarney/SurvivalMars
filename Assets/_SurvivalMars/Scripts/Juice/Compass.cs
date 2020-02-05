6626ee9d29c4110c9544d1740f2c9654fcc3f031
TylerTam <46612488+TylerTam@users.noreply.github.com>
2020-02-05 13:16:04 -0500

    Added compass

---
+++ Assets/_SurvivalMars/Scripts/Juice/Compass.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.identity; 
+    }
+
+}
